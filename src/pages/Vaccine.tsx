2869df3b8f34aef76225700db4cdf5a9d8c70a88
TomLemelle <tom.lemelle@hotmail.fr>
2021-06-29 21:56:47 +0200

    vaccineComponent done

---
+++ src/pages/Vaccine.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navigation from '../components/Nav/Navigation';
+import VaccineComponent from '../components/Vaccine/VaccineComponent';
+
+const Vaccine = () => {
+    return (
+        <div>
+            <Navigation />
+            <VaccineComponent />
+        </div>
+    );
+};
+
+export default Vaccine;
